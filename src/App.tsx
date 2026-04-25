37926b6e2409a28f556c84dba027770ed34dcd4d
kirpichenkoa <kirpichenkoa@eurotorg.by>
2026-04-25 12:30:03 +0300

    1229

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,18 @@ function App() {
 
   return (
     <>
-      <div className='sticky h-18 bg-[#062247] flex justify-center'>
+      <div className='sticky h-18 bg-[#062247] flex justify-center flex-col items-center'>
         <h2 className='text-white text-lg font-semibold'>История</h2>
-      </div> 
+
+        <div className='flex w-full px-10 pt-4'>
+          <div className='border w-full h-6 text-center text-white rounded-l border-[#53678A]'>
+            <span>История ставок</span>
+          </div>
+          <div className='border-r border-t border-b w-full h-6 text-center text-white rounded-r bg-[#53678A] border-[#53678A]'>
+             <span>История игрового счета</span>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
