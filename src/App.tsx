97a34dc24c52c6b52b40b8ba12288aa27ea94c96
kirpichenkoa <kirpichenkoa@eurotorg.by>
2026-04-25 13:58:30 +0300

    1358

--- src/App.tsx
+++ src/App.tsx
@@ -15,28 +15,28 @@ const data: Day[] = [
         amount: 14.21,
         сurrentBalance: 14.21,
         date: "21:27",
-        type: "plus"
+        type: "plus",
       },
       {
         title: "Перевод на игровой счет (ЕРИП)",
         amount: 40.40,
         сurrentBalance: 40.40,
         date: "21:11",
-        type: "plus"
+        type: "plus",
       },
       {
         title: "Перевод на игровой счет (ЕРИП)",
         amount: 12.97,
         сurrentBalance: 12.97,
         date: "20:13",
-        type: "plus"
+        type: "plus",
       },
       {
         title: "Перевод на игровой счет (ЕРИП)",
         amount: 170.00,
         сurrentBalance: 170.00,
         date: "17:58",
-        type: "plus"
+        type: "plus",
       },
     ]
   },
@@ -48,7 +48,108 @@ const data: Day[] = [
         amount: 7.48,
         сurrentBalance: 7.54,
         date: "20:07",
-        type: "plus"
+        type: "plus",
+      },
+      {
+        title: "Перевод на игровой счет (ЕРИП)",
+        amount: 10.00,
+        сurrentBalance: 10.06,
+        date: "19:16",
+        type: "plus",
+      },
+      {
+        title: "Снятие с игрового счета",
+        amount: 24.00,
+        сurrentBalance: 0.06,
+        date: "18:49",
+        type: "minus",
+      },
+      {
+        title: "Перевод на игровой счет (ЕРИП)",
+        amount: 2.67,
+        сurrentBalance: 2.67,
+        date: "15:18",
+        type: "plus",
+      },
+      {
+        title: "Снятие с игрового счета",
+        amount: 23.96,
+        сurrentBalance: 10.00,
+        date: "08:48",
+        type: "minus",
+      },
+    ]
+  },
+  {
+    date: 'Среда, 12 ноября 2025',
+    transactions: [
+      {
+        title: "Перевод на игровой счет (ЕРИП)",
+        amount: 15.00,
+        сurrentBalance: 15.00,
+        date: "19:43",
+        type: "plus",
+      },
+      {
+        title: "Перевод на игровой счет (ЕРИП)",
+        amount: 80.00,
+        сurrentBalance: 80.00,
+        date: "18:33",
+        type: "plus",
+      },
+      {
+        title: "Перевод на игровой счет (ЕРИП)",
+        amount: 31.71,
+        сurrentBalance: 31.71,
+        date: "12:56",
+        type: "plus",
+      },
+      {
+        title: "Перевод на игровой счет (ЕРИП)",
+        amount: 350.00,
+        сurrentBalance: 350.00,
+        date: "12:11",
+        type: "plus",
+      },
+      {
+        title: "Перевод на игровой счет (ЕРИП)",
+        amount: 9.15,
+        сurrentBalance: 82.65,
+        date: "11:08",
+        type: "plus",
+      },
+      {
+        title: "Перевод на игровой счет (ЕРИП)",
+        amount: 390.00,
+        сurrentBalance: 390.10,
+        date: "08:38",
+        type: "plus",
+      },
+    ]
+  },
+  {
+    date: 'Вторник, 11 ноября 2025',
+    transactions: [
+      {
+        title: "Перевод на игровой счет (ЕРИП)",
+        amount: 390.00,
+        сurrentBalance: 390.00,
+        date: "08:59",
+        type: "plus",
+      },
+      {
+        title: "Перевод на игровой счет (ЕРИП)",
+        amount: 30.00,
+        сurrentBalance: 30.00,
+        date: "08:39",
+        type: "plus",
+      },
+      {
+        title: "Перевод на игровой счет (ЕРИП)",
+        amount: 230.00,
+        сurrentBalance: 230.00,
+        date: "08:26",
+        type: "plus",
       },
     ]
   }
