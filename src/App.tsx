4c1d271598354774388429ed796afc28787f9a51
kirpichenkoa <kirpichenkoa@eurotorg.by>
2026-04-25 13:05:56 +0300

    1305

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import './App.css'
+import { Transaction } from './transaction'
 
 function App() {
 
   return (
     <>
-      <div className='sticky h-18 bg-[#062247] flex justify-center flex-col items-center'>
+      <div className='sticky h-20 bg-[#062247] flex justify-center flex-col items-center'>
         <h2 className='text-white text-lg font-semibold'>История</h2>
 
         <div className='flex w-full px-3 pt-1'>
-          <div className='border w-full h-6 text-center text-white rounded-l border-[#53678A]'>
+          <div className='border w-full h-8 text-center text-white rounded-l border-[#53678A]  align-middle'>
             <span className='text-xs'>История ставок</span>
           </div>
-          <div className='border-r border-t border-b w-full h-6 text-center text-white rounded-r bg-[#53678A] border-[#53678A]'>
+          <div className='border-r border-t border-b w-full h-8 text-center text-white rounded-r bg-[#53678A] border-[#53678A] align-middle'>
              <span className='text-xs'>История игрового счета</span>
           </div>
         </div>
+      </div>
+      <div>
+         <Transaction 
+            title="Перевод на игровой счет (ЕРИП)" 
+            amount={14.21} 
+            сurrentBalance={14.21}
+            date="21:27" 
+            type="plus" 
+          />
       </div>
     </>
   )
---
+++ src/transaction.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+type TransactionProp = {
+    title: string,
+    date: string,
+    amount: number,
+    сurrentBalance: number,
+    type: 'plus' | 'minus',
+}
+
+export const Transaction = ({ title, date, amount, type, сurrentBalance }: TransactionProp) => {
+    return (
+        <div className="px-3 pt-3 grid-rows-3 w-full border-b border-stone-300 pb-2">
+            <p className="text-xs text-zinc-400 font-sans row-start-1">{date}</p>
+            <div className="inline-flex justify-between w-full row-start-2">
+                <p>{title}</p>
+                <p className={twMerge(
+                    "font-semibold",
+                    type == 'plus' ? "text-green-600" : "text-red-500"
+                    )}>{amount.toFixed(2)} BYN
+                </p> 
+            </div>
+            <p className="text-xs text-zinc-400 font-sans row-start-3">Баланс: {сurrentBalance}</p>
+        </div>
+    )
+}
