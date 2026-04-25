482555d56c21827e9296db4e9a48aa6210fbcab1
kirpichenkoa <kirpichenkoa@eurotorg.by>
2026-04-25 12:31:10 +0300

    1231

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ function App() {
       <div className='sticky h-18 bg-[#062247] flex justify-center flex-col items-center'>
         <h2 className='text-white text-lg font-semibold'>История</h2>
 
-        <div className='flex w-full px-10 pt-4'>
+        <div className='flex w-full px-3 pt-4'>
           <div className='border w-full h-6 text-center text-white rounded-l border-[#53678A]'>
-            <span>История ставок</span>
+            <span className='text-sm'>История ставок</span>
           </div>
           <div className='border-r border-t border-b w-full h-6 text-center text-white rounded-r bg-[#53678A] border-[#53678A]'>
-             <span>История игрового счета</span>
+             <span className='text-sm'>История игрового счета</span>
           </div>
         </div>
       </div>
